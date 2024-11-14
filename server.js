a96ab7c6a3f5b4dab3cb63645d4c777165afe0c8
dl512 <davidlau512@gmail.com>
2024-11-14 23:16:30 +0800

    change from bcrypt to bcryptjs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 app.use(cors());
